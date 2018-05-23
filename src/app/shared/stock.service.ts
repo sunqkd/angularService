95ed9d8ce046b1a941832eac030bdc13583c1250
sunqkd <sunzhaoqkd@163.com>
2018-05-23 19:47:34 +0800

    'service'

---
+++ src/app/shared/stock.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class StockService {
+
+  	constructor() { }
+
+	getStock() :Stock{
+		return new Stock(1, "IBM");
+	}
+}
+
+export class Stock {
+	constructor(public id:number,public name:string){
+
+	}
+}
